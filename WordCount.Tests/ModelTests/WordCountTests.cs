8746d08904464a583946c344605683f2cc4979a5
Matt Smith <mattsmithereens@gmail.com>
2018-07-04 15:40:11 -0700

    initial commit.  re-started project from scratch.  setters/getters tested PASSED

---
+++ WordCount.Tests/ModelTests/WordCountTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WordCount.Models;
+using System;
+
+
+
+namespace WordCount.Tests.ModelTests
+{   
+    [TestClass]
+    public class WordCountTests
+    {
+        [TestMethod]
+        public void GetSetUserWord_GetsSetsUserWord_String()
+        {
+            //word setter PASSED
+            //Arrange
+            WordCounter testWordCount = new WordCounter();
+            string testWord = "crash";
+
+            //Act
+            testWordCount.SetWord(testWord);
+
+            //Assert
+            Assert.AreEqual("crash", testWordCount.GetWord(testWord));
+        }
+
+        [TestMethod]
+        public void GetSetUserWords_GetsSetsUserWords_String()
+        {
+            //word setter PASSED
+            //Arrange
+            WordCounter testWordCount = new WordCounter();
+            string testWord = "bang bang bang";
+
+            //Act
+            testWordCount.SetWords(testWord);
+
+            //Assert
+            Assert.AreEqual("bang bang bang", testWordCount.GetWords(testWord));
+        }
+    }
+}
---
+++ WordCount/Models/WordCount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WordCount.Models
+{
+    public class WordCounter
+    {
+        private string _word;               //word
+        private string _words;              //comparative word
+        //private string[] _wordArray;        //comparative word array
+        //private string _match;              //word match
+        //private int _wordOccur = 0;         //word match score/occurences
+
+        public void SetWord(string word)
+        {
+            _word = word;
+        }
+
+        public string GetWord(string word)
+        {
+            return _word;
+        }
+
+        public void SetWords(string words)
+        {
+            _words = words;
+        }
+
+        public string GetWords(string words)
+        {
+            return _words;
+        }
+
+    }
+}
