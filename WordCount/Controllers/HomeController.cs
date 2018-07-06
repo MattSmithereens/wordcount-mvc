4e306e8475080499330aa788526d09a7ab8e4e2d
Matt Smith <mattsmithereens@gmail.com>
2018-07-06 16:24:04 -0700

    tidy and finish homecontroller.

--- WordCount/Controllers/HomeController.cs
+++ WordCount/Controllers/HomeController.cs
@@ -9,70 +9,18 @@ namespace WordCount.Controllers
 {
     public class HomeController : Controller
     {
-
-
-
+        
         public IActionResult Index()
         {
             return View();
         }
 
 
-        [HttpGet("/new-counter")]
-        public ActionResult NewCritter()
+        [HttpGet("/counter")]
+        public ActionResult counter()
         {
             return View();
         }
-
-        /*
-        [HttpGet("/critters")]
-        public ActionResult Critters()
-        {
-            List<Critter> allCritters = new List<Critter>();
-            allCritters = Critter.GetAll();
-            return View(allCritters);
-        }
-
-        [HttpPost("/critters")]
-        public ActionResult Create()
-        {
-            Critter newCritter = new Critter(Request.Form["new-critter"]);
-            return RedirectToAction("critters");
-        }
-
-        [HttpGet("/critters/{id}")]
-        public ActionResult CritterDetail(int id)
-        {
-            Critter newCritter = Critter.Find(id);
-            return View(newCritter);
-        }
-
-        [HttpGet("/critters/{id}/rest")]
-        public ActionResult Rest(int id)
-        {
-            Critter newCritter = Critter.Find(id);
-            newCritter.Rest();
-            Critter.PassTime();
-            return View(Critter.Find(id));
-        }
-
-        [HttpGet("/critters/{id}/feed")]
-        public ActionResult Feed(int id)
-        {
-            Critter newCritter = Critter.Find(id);
-            newCritter.Feed();
-            Critter.PassTime();
-            return View(Critter.Find(id));
-        }
-
-        [HttpGet("/critters/{id}/play")]
-        public ActionResult Play(int id)
-        {
-            Critter newCritter = Critter.Find(id);
-            newCritter.Play();
-            Critter.PassTime();
-            return View(Critter.Find(id));
-        }
-*/        
+               
     }
 }
--- WordCount/Controllers/ModelsController.cs
+++ WordCount/Controllers/ModelsController.cs
@@ -6,8 +6,59 @@ using Microsoft.AspNetCore.Mvc;
 using WordCount.Models;
 
 namespace WordCount.Controllers
-{
-    public class ModelsController : Controller
-    { 
+    {
+        public class ModelsController : Controller
+        {
+
+            /*
+            [HttpGet("/critters")]
+            public ActionResult Critters()
+            {
+                List<Critter> allCritters = new List<Critter>();
+                allCritters = Critter.GetAll();
+                return View(allCritters);
+            }
+
+            [HttpPost("/critters")]
+            public ActionResult Create()
+            {
+                Critter newCritter = new Critter(Request.Form["new-critter"]);
+                return RedirectToAction("critters");
+            }
+
+            [HttpGet("/critters/{id}")]
+            public ActionResult CritterDetail(int id)
+            {
+                Critter newCritter = Critter.Find(id);
+                return View(newCritter);
+            }
+
+            [HttpGet("/critters/{id}/rest")]
+            public ActionResult Rest(int id)
+            {
+                Critter newCritter = Critter.Find(id);
+                newCritter.Rest();
+                Critter.PassTime();
+                return View(Critter.Find(id));
+            }
+
+            [HttpGet("/critters/{id}/feed")]
+            public ActionResult Feed(int id)
+            {
+                Critter newCritter = Critter.Find(id);
+                newCritter.Feed();
+                Critter.PassTime();
+                return View(Critter.Find(id));
+            }
+
+            [HttpGet("/critters/{id}/play")]
+            public ActionResult Play(int id)
+            {
+                Critter newCritter = Critter.Find(id);
+                newCritter.Play();
+                Critter.PassTime();
+                return View(Critter.Find(id));
+            }
+    */
+        }
     }
-}
