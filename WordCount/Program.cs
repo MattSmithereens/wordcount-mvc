623219af1398b5f0cb7e00ecfe88dca6544dcfbb
Matt Smith <mattsmithereens@gmail.com>
2018-07-06 11:37:13 -0700

    add Program.cs, Startup.cs

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WordCount
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
