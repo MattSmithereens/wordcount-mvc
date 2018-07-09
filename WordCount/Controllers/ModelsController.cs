d41cc4960ed6e82e156f9b2bf8bafe7ddeb2cf0e
Matt Smith <mattsmithereens@gmail.com>
2018-07-08 22:55:35 -0700

    update README

--- WordCount/Controllers/ModelsController.cs
+++ WordCount/Controllers/ModelsController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WordCount.Models;
 
@@ -9,14 +8,36 @@ namespace WordCount.Controllers
 {
     public class ModelsController : Controller
     {
-
+        
         [Route("/formLib")]
         public ActionResult WordCompare()
         {
+            // display inputted words in result sentence
             WordCounter inputVariable = new WordCounter();
             inputVariable.SetWord(Request.Query["checkWord"]);
             inputVariable.SetWords(Request.Query["checkPhrase"]);
-            return View("result", inputVariable);
+            //if (GetWord("checkword") == "")
+            //{
+            //    return View("error");
+            //}
+            //    else
+            //{
+                return View("result", inputVariable);
+            //}
         }
+
+        // Breaks build
+
+        //[HttpPost("/result")]
+        //public ActionResult WordInput()
+        //{
+        //    WordCounter solution = new WordCounter();
+        //    solution.SetWord(Request.Query["checkWord"]);
+        //    solution.SetWords(Request.Query["checkPhrase"]);
+        //    string lowCaseWord = solution.LowCaseWord();
+        //    string[] compareWords = solution.splitString(cutWords);
+        //    solution.WordOccurs(compareWords, lowCaseWord);
+        //    return View(result);
+        //}
     }
 }
